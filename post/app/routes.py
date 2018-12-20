32caa1903f01ab2652cfef4ab88d511ff7cb6dad
Viacheslav <mrSlavik92@gmail.com>
2018-12-21 00:25:00 +0200

    Update Post-service v2
    add checking null in json, add return for UI, other minor edits

--- post/app/routes.py
+++ post/app/routes.py
@@ -1,47 +1,79 @@
 from app import app
 import flask
 from flask import Flask, jsonify, request
-import requests
 from requests import post
 import json
 import os
+
+link = str(os.getenv("DB_URL"))
 
 @app.route('/posting', methods=['POST'])
 def posting():
 
     def send_json1(sendjson1):
-        sendjson1 = post("http://"+str(os.getenv("DB_URL"))+":5002/set_department", json=sendjson1, headers={"Content-type": "application/json"})
+        # department send function
+        sendjson1 = post("http://" + link + ":5002/set_department", json=sendjson1, headers={"Content-type": "application/json"})
         print(sendjson1.text)
     def send_json2(sendjson2):
-        sendjson2 = post("http://"+str(os.getenv("DB_URL"))+":5002/set_team", json=sendjson2, headers={"Content-type": "application/json"})
+        # Team send function
+        sendjson2 = post("http://" + link + ":5002/set_team", json=sendjson2, headers={"Content-type": "application/json"})
         print(sendjson2.text)
     def send_json3(sendjson3):
-        sendjson3 = post("http://"+str(os.getenv("DB_URL"))+":5002/set_employee", json=sendjson3, headers={"Content-type": "application/json"})
+        # Employee send function
+        sendjson3 = post("http://" + link + ":5002/set_employee", json=sendjson3, headers={"Content-type": "application/json"})
         print(sendjson3.text)
+
+    def json_count(little_json):
+        # Check null in json function
+        len_keys = len(little_json)
+        len_value = 0
+        for item in little_json:
+            n = little_json[item]
+            if n == None:
+                len_value = 0
+            else:
+                len_value += 1
+
+        if len_value == len_keys:
+            return True
+        else:
+            return False
 
     datajson = request.data
     data = json.loads(datajson)
+    msg = "Successfully sent"
 
     for i in data:
         if i == "department":
             Department = data["department"]
-            if Department == None:
+            if Department is None:
                 pass
             else:
-                send_json1(Department)
+                if json_count(Department) is True:
+                    send_json1(Department)
+                else:
+                    msg = "Fill in all the fields"
+
         elif i == "team":
             Team = data["team"]
-            if Team == None:
+            if Team is None:
                 pass
             else:
-                send_json2(Team)
+                if json_count(Team) is True:
+                    send_json2(Team)
+                else:
+                    msg = "Fill in all the fields"
+
         elif i == "employee":
             Employee = data["employee"]
-            if Employee == None:
+            if Employee is None:
                 pass
             else:
-                send_json3(Employee)
+                if json_count(Employee) is True:
+                    send_json3(Employee)
+                else:
+                    msg = "Fill in all the fields"
         else:
             pass
-            
-    return ""
+
+    return jsonify(msg)
